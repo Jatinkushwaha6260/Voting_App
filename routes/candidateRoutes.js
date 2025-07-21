eb2f954f588ae90581d735a11c973b9ef6d6d900
Jatin Kushwaha <jatinkushwaha995@gmail.com>
2025-07-21 10:42:25 +0530

    Third commit

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -95,6 +95,75 @@ router.delete('/:candidateId' , jwtAuthMiddleware , async (req , res) => {
 });
 
 // Let's started voting
+router.post('/vote/:candidateId' , jwtAuthMiddleware , async (req , res) => {
+    // no admin can vote
+    // user can only vote once
+    candidateId = req.params.candidateId;
+    userId = req.user.id;
+
+    try{
+        // Find the candidate document with the specified candidateId
+        const candidate = await Candidate.findById(candidateId);
+        if(!candidate){
+            return res.status(404).json({message: 'candidate not found'});
+
+        }
+        
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message: 'user not found'});
+        }
+
+        if(user.isVoted){
+            return res.status(400).json({message: 'you have already voted'});
+        }
+
+        if(user.role == 'admin'){
+            return res.status(403).json({message: 'admin is not allowed'});
+        }
+
+        // Update the candidate document to record the vote
+        candidate.votes.push({user: userId});
+        candidate.voteCount++;
+        await candidate.save();
+
+        // Update the user document
+        user.isVoted = true;
+        await user.save();
+
+        res.status(200).json({message: 'vote recorded successfully'});
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+
+    }
+});
+
+
+// Find vote count
+router.get('/vote/count' , async (req , res) => {
+    try{
+        // Find all candidates and sort them by voteCount in decending order
+        const candidate = await Candidate.find().sort({voteCount: 'desc'});
+
+       // Map the candidates to only return their name and voteCount
+       const voteRecord = candidate.map((data) => {
+        return {
+            party: data.party,
+            voteCount: data.voteCount
+        }
+       });
+
+       return res.status(200).json(voteRecord);
+
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+
 
 
 
